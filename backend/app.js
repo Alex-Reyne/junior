cf5bc1456530db13a2816fc8bed7db252dc81b31
Alex-Reyne <creativereyne@gmail.com>
2022-02-06 16:10:17 -0600

    SECRET???>??ASDF>S>AFD>SA?F>?>??>?>>>>>>??>?>?

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+//
 app.use(
 	session({
 		secret: process.env.SESSION_SECRET,

36ea389bd08c94168fad0f9b2bfa870dcdd3a2e9
Alex-Reyne <creativereyne@gmail.com>
2022-02-06 15:48:13 -0600

    change client connection to dotenv database_url

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true,
+});
 
 console.log(`Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}`);
 client.connect();

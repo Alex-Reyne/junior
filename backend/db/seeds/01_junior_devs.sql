f3f9e8e9eaad3102c86ddc90712a24621c1d421c
Alex-Reyne <creativereyne@gmail.com>
2022-02-06 18:50:29 -0600

    remove real info from junior dev seeds

--- backend/db/seeds/01_junior_devs.sql
+++ backend/db/seeds/01_junior_devs.sql
@@ -3,145 +3,100 @@ INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city,
   'Lighthouse',
   'larrylighthouse@junior.com',
   '123',
-  '243.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   'Student. Coffee maven. Hardcore food nerd. Amateur music fanatic. Problem solver. Travel fan. Total web enthusiast.',
   'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-  'https://github.com/karolinaCodes',
-  'https://www.linkedin.com/in/karolina-swislocki-0662b0210/',
-  'https://resume.creddle.io/resume/gf5ixfvh8s6'
-);
-INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, photo_url, github_url, linkedin_url, resume_url) VALUES (
-  'Karolina',
-  'Swislocki',
-  'karolinaswislocki@hotmail.com',
-  '123',
-  '306.325.1554',
-  'Toronto',
-  'Full-stack Developer',
-  'Student. Coffee maven. Hardcore food nerd. Amateur music fanatic. Problem solver. Travel fan. Total web enthusiast.',
-  'https://media-exp1.licdn.com/dms/image/C4E03AQF3EU6ck0WYiw/profile-displayphoto-shrink_800_800/0/1641490468974?e=1648684800&v=beta&t=Tk3YA3pss1wnMzPgsNnkyS23hbTI3bSiG3hrvbXD-aE',
-  'https://github.com/karolinaCodes',
-  'https://www.linkedin.com/in/karolina-swislocki-0662b0210/',
-  'https://resume.creddle.io/resume/gf5ixfvh8s6'
-);
-INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, photo_url, github_url, linkedin_url, resume_url) VALUES (
-  'Alexander',
-  'Reyne',
-  'creativereyne@gmail.com',
-  '123',
-  '845.821.8563',
-  'Saskatoon',
-  'Full-stack Developer',
-  'Full-Stack web developer and freelance graphic designer with 11 years of experience.',
-  'https://avatars.githubusercontent.com/u/85032662?s=400&u=74436a897a8b39116013a1950b272e57fc214b75&v=4',
-  'https://github.com/Alex-Reyne',
-  'https://www.linkedin.com/in/alexanderreyne/',
-  'https://drive.google.com/file/d/1qwTq0XySY2WjYinKWsfmt2BhM_ztlNmN/view?usp=sharing'
-);
-INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, photo_url, github_url, linkedin_url, resume_url) VALUES (
-  'Sarah',
-  'Dela Cruz',
-  'sarah.delacruz@gmail.com',
-  '123',
-  '456.1678.4567',
-  'Kitchener',
-  'Full-stack Developer',
-  'Solution-driven full stack web developer with an eye for design and a passion for client satisfaction.',
-  'https://avatars.githubusercontent.com/u/6501508?s=400&u=18add3e591596026b565eccc22106f545a836ce8&v=4',
-  'https://github.com/sarahdeecee',
-  'https://www.linkedin.com/in/sarah-delacruz/',
-  'https://resume.io/r/eF4em6lp3'
+  'https://github.com/',
+  'https://www.linkedin.com/',
+  'https://resume.creddle.io/'
 );
 INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, github_url, linkedin_url, resume_url) VALUES (
   'Sergio',
   'Holm',
   '4@junior.com',
   '123',
-  '306.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   '',
-  'https://github.com/karolinaCodes',
-  'https://www.linkedin.com/in/karolina-swislocki-0662b0210/',
-'https://resume.creddle.io/resume/gf5ixfvh8s6'
+  'https://github.com/',
+  'https://www.linkedin.com/',
+'https://resume.creddle.io/'
 );
 INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, github_url, linkedin_url, resume_url) VALUES (
   'Klemen',
   'Hackett',
   '5@junior.com',
   '123',
-  '306.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   '',
-  'https://github.com/karolinaCodes',
-  'https://www.linkedin.com/in/karolina-swislocki-0662b0210/',
-'https://resume.creddle.io/resume/gf5ixfvh8s6'
+  'https://github.com/',
+  'https://www.linkedin.com/',
+'https://resume.creddle.io/'
 );
 INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, github_url, linkedin_url, resume_url) VALUES (
   'Apollonia',
   'Beitel',
   '6@junior.com',
   '123',
-  '306.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   '',
-  'https://github.com/karolinaCodes','http
-  s://www.linkedin.com/in/karolina-swislocki-0662b0210/',
-'https://resume.creddle.io/resume/gf5ixfvh8s6'
+  'https://github.com/','http
+  s://www.linkedin.com/',
+'https://resume.creddle.io/'
 );
 INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, github_url, linkedin_url, resume_url) VALUES (
   'Gvidas',
   'Antonov',
   '7@junior.com',
   '123',
-  '306.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   '',
-  'https://github.com/karolinaCodes',
-  'https://www.linkedin.com/in/karolina-swislocki-0662b0210/',
-'https://resume.creddle.io/resume/gf5ixfvh8s6'
+  'https://github.com/',
+  'https://www.linkedin.com/',
+'https://resume.creddle.io/'
 );
 INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, github_url, linkedin_url, resume_url) VALUES (
   'Titus',
   'Lukeson',
   '8@junior.com',
   '123',
-  '306.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   '',
-  'https://github.com/karolinaCode
-  s','https://www.linkedin.com/in/karolina-swislock
-i-0662b0210/','https://resume.creddle.io/resume/gf5ixfvh8s6'
+  'https://github.com/
+  s','https://www.linkedin.com/in/','https://resume.creddle.io/'
 );
 INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, github_url, linkedin_url, resume_url) VALUES (
   'Pylyp',
   'Gates',
   '9@junior.com',
   '123',
-  '306.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   '',
-  'https://github.com/karolinaCode
-  s','https://www.linkedin.com/in/karolina-swislock
-i-0662b0210/','https://resume.creddle.io/resume/gf5ixfvh8s6'
+  'https://github.com/
+  s','https://www.linkedin.com/in/','https://resume.creddle.io/'
 );
 INSERT INTO junior_devs (first_name,last_name,email,password,phone_number, city, headline, bio, github_url, linkedin_url, resume_url) VALUES (
   'Senan',
   'Thompson',
   '10@junior.com',
   '123',
-  '306.821.3553',
+  '243.146.5555',
   'Toronto',
   'Full-stack Developer',
   '',
-  'https://github.com/karolinaCode
-  s','https://www.linkedin.com/in/karolina-swislock
-i-0662b0210/','https://resume.creddle.io/resume/gf5ixfvh8s6'
+  'https://github.com/
+  s','https://www.linkedin.com/in/','https://resume.creddle.io/'
 );

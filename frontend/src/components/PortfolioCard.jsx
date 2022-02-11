ec57190f08bc0d0a238372823376567e0841b867
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-02-10 19:54:34 -0500

    add kebab to portfolio card

--- frontend/src/components/PortfolioCard.jsx
+++ frontend/src/components/PortfolioCard.jsx
@@ -1,15 +1,26 @@
 import './styles/PortfolioCard.scss';
-import { CardContent, CardMedia } from '@mui/material';
+import { CardContent, CardHeader, CardMedia, IconButton } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { UserContext } from '../Providers/userProvider';
+import { useContext } from 'react';
 
 export default function Profile(props) {
 	const { title, description, thumbnail_photo_url, github_link, live_link } =
 		props;
 	const imgUrl =
 		'https://cdn.dribbble.com/users/409537/screenshots/14290034/media/965f91e1549a177acd63b8dced7592fa.png?compress=1&resize=1200x900&vertical=top';
+	const { currentUser } = useContext(UserContext);
 
 	return (
 		<CardContent>
-			<h1>{title ? title : 'Untitled Project'}</h1>
+			<CardHeader
+				title={title ? title : 'Untitled Project'}
+				action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+			/>
 			<CardMedia
 				component='img'
 				image={thumbnail_photo_url ? thumbnail_photo_url : imgUrl}
